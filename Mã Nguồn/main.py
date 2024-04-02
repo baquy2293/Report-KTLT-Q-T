9fc0cf78b5802f147731e68f04264a7a6ffb0a3c
baquy2293 <op347766gmail.com>
2024-04-02 15:38:36 +0700

    add doc

--- Mã Nguồn/main.py
+++ Mã Nguồn/main.py
@@ -1,109 +1,108 @@
 import datetime
-from datetime import date
 from tkinter import *
 from tkinter import messagebox
 import os
 from tkinter import ttk
 def create_user():
-    # current_date = datetime.now()
-    # print(current_date)
-    #
-    # # Định dạng ngày thành chuỗi theo định dạng '%d.%m.%Y'
-    # formatted_date = current_date.strftime('%d.%m.%Y')
-    command = 'pytest src/check_response/Create_user.py --html="report/Create_user.html"'
+    d = datetime.date.today()
+    command = f'pytest src/check_response/Create_user.py --html="report/{d}/Create_user.html"'
     os.system(command)
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def delete_user():
-    os.system("pytest src/check_response/Delete_user.py --html=report/Delete_user.html")
+    os.system(f"pytest src/check_response/Delete_user.py --html=report/{datetime.date.today()}/Delete_user.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def get_user():
-    os.system("pytest src/check_response/Get_users.py --html=report/$(date +%d.%m.%Y)/Get_user.html")
+    os.system(f"pytest src/check_response/Get_users.py --html=report/{datetime.date.today()}/Get_user.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 def update_user():
-    os.system("pytest src/check_response/Update_user.py --html=report/$(date +%d.%m.%Y)/Update_user.html")
+    os.system(f"pytest src/check_response/Update_user.py --html=report/{datetime.date.today()}/Update_user.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def login():
-    os.system("pytest src/check_response/Login.py --html=report/$(date +%d.%m.%Y)/Login.html")
+    os.system(f"pytest src/check_response/Login.py --html=report/{datetime.date.today()}/Login.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def register():
-    os.system("pytest src/check_response/Register.py --html=report/$(date +%d.%m.%Y)/Register.html")
+    os.system(f"pytest src/check_response/Register.py --html=report/{datetime.date.today()}/Register.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def dos():
-    os.system("pytest src/security/Dos.py --html=report/$(date +%d.%m.%Y)/DOS.html")
+    os.system(f"pytest src/security/Dos.py --html=report/{datetime.date.today()}/DOS.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def sql_injection():
-    os.system("pytest src/security/SQL_Injection.py --html=report/$(date +%d.%m.%Y)/SQL_Injection.html")
+    os.system(f"pytest src/security/SQL_Injection.py --html=report/{datetime.date.today()}/SQL_Injection.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
 def all():
-    os.system("pytest src/check_response/Create_user.py --html=report/$(date +%d.%m.%Y)/Create_user.html")
-    os.system("pytest src/check_response/Delete_user.py --html=report/$(date +%d.%m.%Y)/Delete_user.html")
-    os.system("pytest src/check_response/Get_users.py --html=report/$(date +%d.%m.%Y)/Get_user.html")
-    os.system("pytest src/check_response/Update_user.py --html=report/$(date +%d.%m.%Y)/Update_user.html")
-    os.system("pytest src/check_response/Login.py --html=report/$(date +%d.%m.%Y)/Login.html")
-    os.system("pytest src/check_response/Register.py --html=report/$(date +%d.%m.%Y)/Register.html")
-    os.system("pytest src/security/Dos.py --html=report/$(date +%d.%m.%Y)/DOS.html")
-    os.system("pytest src/security/SQL_Injection.py --html=report/$(date +%d.%m.%Y)/SQL_Injection.html")
+    os.system(f"pytest src/check_response/Create_user.py --html=report/{datetime.date.today()}//Create_user.html")
+    os.system(f"pytest src/check_response/Delete_user.py --html=report/{datetime.date.today()}//Delete_user.html")
+    os.system(f"pytest src/check_response/Get_users.py --html=report/{datetime.date.today()}//Get_user.html")
+    os.system(f"pytest src/check_response/Update_user.py --html=report/{datetime.date.today()}//Update_user.html")
+    os.system(f"pytest src/check_response/Login.py --html=report/{datetime.date.today()}//Login.html")
+    os.system(f"pytest src/check_response/Register.py --html=report/{datetime.date.today()}//Register.html")
+    os.system(f"pytest src/security/Dos.py --html=report/{datetime.date.today()}//DOS.html")
+    os.system(f"pytest src/security/SQL_Injection.py --html=report/{datetime.date.today()}//SQL_Injection.html")
     messagebox.showinfo("All Task Completed", "All Task completed!")
-root = Tk()
-root.title("Testing API")
-
-root.option_add("*Font", "Arial")  # thêm dòng này để đổi font chữ thành Arial
-
-style = ttk.Style()
-style.configure('TButton', font=('Arial', 14), foreground="black", background="light blue", width=20)
-
-label = Label(root, text="Welcome to the Testing API!", font=("Arial", 16))
-label.pack(pady=10)
-
-frame1 = Frame(root)
-frame1.pack(pady=10)
-
-button1 = ttk.Button(frame1, text="Create User", width=20,command=create_user)
-button1.pack(side=LEFT, padx=5)
-
-button2 = ttk.Button(frame1, text="Delete User", width=20,command=delete_user)
-button2.pack(side=LEFT, padx=5)
-
-button3 = ttk.Button(frame1, text="Get User", width=20,command=get_user)
-button3.pack(side=LEFT, padx=5)
-
-button4 = ttk.Button(frame1, text="Update User", width=20,command=update_user)
-button4.pack(side=LEFT, padx=5)
-
-frame2 = Frame(root)
-frame2.pack(pady=10)
-
-button5 = ttk.Button(frame2, text="Login", width=20,command=login)
-button5.pack(side=LEFT, padx=5)
-
-button6 = ttk.Button(frame2, text="Register", width=20,command=register)
-button6.pack(side=LEFT, padx=5)
-
-frame3 = Frame(root)
-frame3.pack(pady=10)
-
-label2 = Label(frame3, text="Security:", font=("Arial", 14))
-label2.pack(side=LEFT, padx=5)
-
-button7 = ttk.Button(frame3, text="Dos", width=10,command=dos)
-button7.pack(side=LEFT, padx=5)
-
-button8 = ttk.Button(frame3, text="SQL Injection", width=15,command=sql_injection)
-button8.pack(side=LEFT, padx=5)
-
-button9 = ttk.Button(root, text="ALL", command=all, width=10)
-button9.pack(pady=10)
 
 
-button9 = ttk.Button(root, text="Quit", command=root.quit, width=10)
-button9.pack(pady=10)
+create_user()
 
-
-root.mainloop()
+# root = Tk()
+# root.title("Testing API")
+#
+# root.option_add("*Font", "Arial")  # thêm dòng này để đổi font chữ thành Arial
+#
+# style = ttk.Style()
+# style.configure('TButton', font=('Arial', 14), foreground="black", background="light blue", width=20)
+#
+# label = Label(root, text="Welcome to the Testing API!", font=("Arial", 16))
+# label.pack(pady=10)
+#
+# frame1 = Frame(root)
+# frame1.pack(pady=10)
+#
+# button1 = ttk.Button(frame1, text="Create User", width=20,command=create_user)
+# button1.pack(side=LEFT, padx=5)
+#
+# button2 = ttk.Button(frame1, text="Delete User", width=20,command=delete_user)
+# button2.pack(side=LEFT, padx=5)
+#
+# button3 = ttk.Button(frame1, text="Get User", width=20,command=get_user)
+# button3.pack(side=LEFT, padx=5)
+#
+# button4 = ttk.Button(frame1, text="Update User", width=20,command=update_user)
+# button4.pack(side=LEFT, padx=5)
+#
+# frame2 = Frame(root)
+# frame2.pack(pady=10)
+#
+# button5 = ttk.Button(frame2, text="Login", width=20,command=login)
+# button5.pack(side=LEFT, padx=5)
+#
+# button6 = ttk.Button(frame2, text="Register", width=20,command=register)
+# button6.pack(side=LEFT, padx=5)
+#
+# frame3 = Frame(root)
+# frame3.pack(pady=10)
+#
+# label2 = Label(frame3, text="Security:", font=("Arial", 14))
+# label2.pack(side=LEFT, padx=5)
+#
+# button7 = ttk.Button(frame3, text="Dos", width=10,command=dos)
+# button7.pack(side=LEFT, padx=5)
+#
+# button8 = ttk.Button(frame3, text="SQL Injection", width=15,command=sql_injection)
+# button8.pack(side=LEFT, padx=5)
+#
+# button9 = ttk.Button(root, text="ALL", command=all, width=10)
+# button9.pack(pady=10)
+#
+#
+# button9 = ttk.Button(root, text="Quit", command=root.quit, width=10)
+# button9.pack(pady=10)
+#
+#
+# root.mainloop()
--- Mã Nguồn/src/check_response/Create_user.py
+++ Mã Nguồn/src/check_response/Create_user.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 
+
 # Test case to verify that the API endpoint URL is correct and accessible
 def test_api_endpoint_accessible():
     data = {"name": "NGUYEN VAN A", "job": "student"}
@@ -15,26 +16,34 @@ def test_api_returns_expected_format():
     response = requests.post("https://reqres.in/api/users", data=data)
     assert response.headers["Content-Type"] == "application/json; charset=utf-8"
     print("Content-Type : OK")
+
+
 # Test case to verify when create user failed
 def test_api_returns_correct_status_code_when_wrong_type_data():
     data = {"name": 1, "job": 2}
-
     response = requests.post("https://reqres.in/api/users", data=data)
     assert response.status_code == 400
     print("Status-code : OK")
+
+
 # test case expected data
 def test_api_returns_expected_data():
-    data = {"name": "NGUYEN VAN A", "job": "student"}
+    data = {
+    "name": "tbq",
+    "job": "student"
+}
     response = requests.post("https://reqres.in/api/users", data=data)
 
     format = "%Y-%m-%dT%H:%M:%S.%fZ"
-    assert response.json()["data"]["name"] == data["name"]
+
+    print(response.json())
+    assert response.json()["name"] == data["name"]
     print("name : OK")
-    assert response.json()["data"]["job"] == data["job"]
+    assert response.json()["job"] == data["job"]
     print("job : OK")
-    assert response.json()["data"]["id"].isdigit() == True
+    assert response.json()["id"].isdigit() == True
     print("id : OK")
-    assert datetime.strptime(response.json()["data"]["createdAt"], format), "Dateformat not as expect"
+    assert datetime.strptime(response.json()["createdAt"], format), "Dateformat not as expect"
     print("createdAt : OK")
 
 
@@ -54,12 +63,9 @@ def test_api_returns_correct_headers():
     print("CF-Cache-Status : OK")
 
 
-
 # Test case to verify that the API endpoint returns data in a reasonable amount of time and does not time out
 def test_api_returns_data_in_reasonable_time():
     data = {"name": "NGUYEN VAN A", "job": "student"}
     response = requests.post("https://reqres.in/api/users", data=data)
     assert response.elapsed.total_seconds() < 5  # Verify that the request took less than 5 seconds
     print("total_time : OK")
-
-
