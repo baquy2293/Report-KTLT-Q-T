8fac5e180c33cead7f061a6cd3d77ea387a04a5d
baquy2293 <op347766gmail.com>
2024-04-05 03:23:02 +0700

    sua UI, doc

--- Mã Nguồn/main.py
+++ Mã Nguồn/main.py
@@ -36,6 +36,9 @@ def sql_injection():
     os.system(f"pytest src/security/SQL_Injection.py --html=report/{datetime.date.today()}/SQL_Injection.html")
     messagebox.showinfo("Task Completed", "Task completed!")
 
+def close_root():
+  root.destroy()
+
 def all():
     os.system(f"pytest src/check_response/Create_user.py --html=report/{datetime.date.today()}//Create_user.html")
     os.system(f"pytest src/check_response/Delete_user.py --html=report/{datetime.date.today()}//Delete_user.html")
@@ -47,62 +50,50 @@ def all():
     os.system(f"pytest src/security/SQL_Injection.py --html=report/{datetime.date.today()}//SQL_Injection.html")
     messagebox.showinfo("All Task Completed", "All Task completed!")
 
+root = Tk()
+root.title("Testing API")
+root.geometry("800x400")
 
-create_user()
+root.option_add("*Font", "Arial")
+root.configure(background='#7D95AB')
 
-# root = Tk()
-# root.title("Testing API")
-#
-# root.option_add("*Font", "Arial")  # thêm dòng này để đổi font chữ thành Arial
-#
-# style = ttk.Style()
-# style.configure('TButton', font=('Arial', 14), foreground="black", background="light blue", width=20)
-#
-# label = Label(root, text="Welcome to the Testing API!", font=("Arial", 16))
-# label.pack(pady=10)
-#
-# frame1 = Frame(root)
-# frame1.pack(pady=10)
-#
-# button1 = ttk.Button(frame1, text="Create User", width=20,command=create_user)
-# button1.pack(side=LEFT, padx=5)
-#
-# button2 = ttk.Button(frame1, text="Delete User", width=20,command=delete_user)
-# button2.pack(side=LEFT, padx=5)
-#
-# button3 = ttk.Button(frame1, text="Get User", width=20,command=get_user)
-# button3.pack(side=LEFT, padx=5)
-#
-# button4 = ttk.Button(frame1, text="Update User", width=20,command=update_user)
-# button4.pack(side=LEFT, padx=5)
-#
-# frame2 = Frame(root)
-# frame2.pack(pady=10)
-#
-# button5 = ttk.Button(frame2, text="Login", width=20,command=login)
-# button5.pack(side=LEFT, padx=5)
-#
-# button6 = ttk.Button(frame2, text="Register", width=20,command=register)
-# button6.pack(side=LEFT, padx=5)
-#
-# frame3 = Frame(root)
-# frame3.pack(pady=10)
-#
-# label2 = Label(frame3, text="Security:", font=("Arial", 14))
-# label2.pack(side=LEFT, padx=5)
-#
-# button7 = ttk.Button(frame3, text="Dos", width=10,command=dos)
-# button7.pack(side=LEFT, padx=5)
-#
-# button8 = ttk.Button(frame3, text="SQL Injection", width=15,command=sql_injection)
-# button8.pack(side=LEFT, padx=5)
-#
-# button9 = ttk.Button(root, text="ALL", command=all, width=10)
-# button9.pack(pady=10)
-#
-#
-# button9 = ttk.Button(root, text="Quit", command=root.quit, width=10)
-# button9.pack(pady=10)
-#
-#
-# root.mainloop()
+style = ttk.Style()
+style.configure('TButton', font=('Arial', 14), foreground="black", background="light blue", width=20)
+
+label = Label(root, text="Welcome to the Testing API!", font=("Arial", 16))
+label.pack(pady=10)
+
+button_create_user = ttk.Button(root, text="Create User", width=20, command=create_user)
+button_create_user.place(x=50, y=50, width=150)
+
+button_delete_user = ttk.Button(root, text="Delete User", width=20, command=delete_user)
+button_delete_user.place(x=230, y=50, width=150)
+
+button_get_user = ttk.Button(root, text="Get User", width=20, command=get_user)
+button_get_user.place(x=410, y=50, width=150)
+
+button_update_user = ttk.Button(root, text="Update User", width=20, command=update_user)
+button_update_user.place(x=590, y=50, width=150)
+
+button_login = ttk.Button(root, text="Login", width=20, command=login)
+button_login.place(x=230, y=100, width=150)
+
+button_register = ttk.Button(root, text="Register", width=20, command=register)
+button_register.place(x=410, y=100, width=150)
+
+label_security = Label(root, text="Security:", font=("Arial", 14))
+label_security.place(x=70, y=150, width=150)
+
+button_dos = ttk.Button(root, text="Dos", width=10, command=dos)
+button_dos.place(x=230, y=150, width=150)
+
+button_sql_injection = ttk.Button(root, text="SQL Injection", width=15, command=sql_injection)
+button_sql_injection.place(x=410, y=150, width=150)
+
+button_all = ttk.Button(root, text="ALL", command=all, width=10)
+button_all.place(x=320, y=200, width=150)
+
+button_quit = ttk.Button(root, text="Quit", command=close_root, width=10)
+button_quit.place(x=320, y=250, width=150)
+
+root.mainloop()
